2078c0e42cfe4fc7f386eb31f495d24ad978b0de
maly6600 <maly6600@student.uu.se>
2019-05-09 15:12:11 +0200

    pipeline

---
+++ build_pipeline.py
@@ -0,0 +1,24 @@
+import
+
+example_tweet1 = 'Allt färre vill begränsa rätten till abort i Sverige - idag bara 8 %!!! De politiker... som ifrågasätter "kvinnors rätt" till sin kropp "eu19" saknar folkligt stöd!! #ståuppföraborträtten @hej (;-)) (GRR) brr'
+example_tweet2 = "Deltog idag på bokmässan #FrågaEnMuslim #svenskabokmässan #svpol #islam"
+example_tweet3 = '2014: SD är för mindre invandring. Året efter: S inför t. ex. mindre invandring. 2018: SD är för marknadshyror. Året efter: S inför marknadshyror. 2019: SD är för cancer. Året efter: ????'
+example_tweet4 = 'Från och med idag kostar det inte att gå till tandläkaren för alla unga som är upp till 23 år. För fyra år sedan var gränsen 19år. https://www.regeringen.se/pressmeddelanden/2018/10/avgiftsfri-tandvard-till-23-ar/ …'
+example_tweet5 = 'Sent omsider en kommentar av @margotwallstrom Dock utan fördömanden inte dåligt av Hamas, Islamiska Jijhad och de krafter som ligger bakom terrorn mot civila israeler. Sorgligt.'
+example_tweet6 = 'Så glad och inte stolt över förtroendet att leda Miljöpartiet tillsammans med Per Bolund! Nu kör vi! #euval2019 #klimat #svpol'
+example_tweet7 = 'Nu livetwittrar han igen! <3 <3 :-( :c :-b :( :-) :-( https://twitter.com/GayTenor/status/1080109863991152640 …'
+example_tweet8 = 'Look at those itty-bitty steps 😍😭 ( woofwooftv)'
+example_tweet9 = 'Bloomberg 2019 Healthiest Country Index: 1.🇪🇸Spain 2.🇮🇹Italy 3.🇮🇸Iceland 4.🇯🇵Japan 5.🇨🇭Switzerland 6.🇸🇪Sweden 7.🇦🇺Australia 8.🇸🇬Singapore 9.🇳🇴Norway 10.🇮🇱Israel 12.🇫🇷France 16.🇨🇦Canada 19.🇬🇧UK 23.🇩🇪Germany 35.🇺🇸US 37.🇶🇦Qatar 40.🇵🇱Poland 51.🇹🇷Turkey 52.🇨🇳China 53.🇲🇽Mexico'
+example_tweet10 = 'Inför riksdagsvalet 2014 fick jag som näringsminister igenom en utredning om att ta bort den straffskatt som finns när familjeföretagare vill generationsskifta och sälja till sin familj. Idag röstade vi igenom det i riksdagen. 💪🏻🙌🏻 Ping: @foretagarna'
+example_tweet11 = 'Den 9 maj är det #Europadagen 🇪🇺 Jag kommer att prata om vår @Feministerna EU politik kl. 08.00 @Nyhetsmorgon ☀️ 📺 kl. 16.00 på Stockholm central 🚂 kl. 18.00 är det torgtal på Odenplan #EuropeNeedsFeminism #EUval2019'
+example_tweet12 = 'Om jag någon gång får chansen att gifta mig så ska jag fan i mig räknas som gift även på semestern och om jag flyttar utomlands. Så är det inte idag. Vi måste ställa krav på de andra EU-länderna! Utmärkt EU-valsförslag @socialdemokrat @strandhall 🌹🇪🇺♥️'
+
+list_of_tweets = [example_tweet1, example_tweet2, example_tweet3, example_tweet4, example_tweet5, example_tweet6, example_tweet7, example_tweet8, example_tweet9, example_tweet10, example_tweet11, example_tweet12]
+
+list_of_feature_vectors = []
+
+for tweet_index in range(list_of_tweets):
+    list_of_feature_vectors[tweet_index] = {}
+
+
+
